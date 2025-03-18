c0dffb3b62ea106c836d6e2e089c30cc83211055
Endi <48561536+endilegend@users.noreply.github.com>
2025-03-18 19:19:56 -0400

    Converted db to be in mysql and also store the articles as paths

--- CIRT.sql
+++ CIRT.sql
@@ -1,30 +1,26 @@
 CREATE DATABASE CIRT;
-
--- ENUM types:
-CREATE TYPE user_role AS ENUM ('Reviewer', 'Author', 'Viewer');
-CREATE TYPE article_status AS ENUM ('Sent', 'Under Review', 'Approved', 'Declined');
-CREATE TYPE article_type AS ENUM ('Article', 'Journal', 'Poster', 'Paper');
+USE CIRT;
 
 CREATE TABLE users (
-                       user_role user_role DEFAULT 'Author' NOT NULL,
-                       f_name VARCHAR(50) NOT NULL,
-                       l_name VARCHAR(50) NOT NULL,
-                       id VARCHAR(250) PRIMARY KEY,
-                       email VARCHAR(100) UNIQUE NOT NULL
+    user_role ENUM('Reviewer', 'Author', 'Viewer') DEFAULT 'Author' NOT NULL,
+    f_name VARCHAR(50) NOT NULL,
+    l_name VARCHAR(50) NOT NULL,
+    id VARCHAR(250) PRIMARY KEY,
+    email VARCHAR(100) UNIQUE NOT NULL
 );
 
 CREATE TABLE article (
-                         id SERIAL PRIMARY KEY,
-                         pdf_file BYTEA NOT NULL,
-                         author_id VARCHAR(250),
-                         status article_status,
-                         type article_type,
-                         FOREIGN KEY (author_id) REFERENCES users(id)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    pdf_path VARCHAR(255) NOT NULL,  -- storing the file path instead of the PDF content
+    author_id VARCHAR(250),
+    status ENUM('Sent', 'Under Review', 'Approved', 'Declined'),
+    type ENUM('Article', 'Journal', 'Poster', 'Paper'),
+    FOREIGN KEY (author_id) REFERENCES users(id)
 );
 
 CREATE TABLE keywords (
-                          id SERIAL PRIMARY KEY,
-                          article_id INT,
-                          keyword VARCHAR(50) NOT NULL,
-                          FOREIGN KEY (article_id) REFERENCES article(id)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    article_id INT,
+    keyword VARCHAR(50) NOT NULL,
+    FOREIGN KEY (article_id) REFERENCES article(id)
 );
