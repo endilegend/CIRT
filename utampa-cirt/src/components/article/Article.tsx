4bc6226018262152f4127d1615a1cef9be65bc75
Endi <troqeendi@gmail.com>
2025-04-13 16:41:21 -0400

    fixing build error

--- utampa-cirt/src/components/article/Article.tsx
+++ utampa-cirt/src/components/article/Article.tsx
@@ -1,1 +1,13 @@
-<TextToSpeech articleId={article.id} />;
+import { TextToSpeech } from "./TextToSpeech";
+
+type MyType = Record<string, unknown>;
+
+interface ArticleProps {
+  article: {
+    id: string;
+  };
+}
+
+export function Article({ article }: ArticleProps) {
+  return <TextToSpeech articleId={article.id} />;
+}
