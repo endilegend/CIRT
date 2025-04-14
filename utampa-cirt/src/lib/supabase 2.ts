e1b4020d0b582099d2057ece2c1b30fa4d66444f
Endi <troqeendi@gmail.com>
2025-04-13 20:29:21 -0400

    fixed supabase config

--- utampa-cirt/src/lib/supabase 2.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://thpwxsppkxnigxysuvpb.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRocHd4c3Bwa3huaWd4eXN1dnBiIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NDIzOTIwNCwiZXhwIjoyMDU5ODE1MjA0fQ.JpXIFJ4UpeWxY7NP86-K3QTzD5SS3qPPBT7qZBtIsGI";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
--- utampa-cirt/src/lib/supabase.ts
+++ utampa-cirt/src/lib/supabase.ts
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://thpwxsppkxnigxysuvpb.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRocHd4c3Bwa3huaWd4eXN1dnBiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI5NjQ0MDAsImV4cCI6MjAyODU0MDQwMH0.8Q5Q5Q5Q5Q5Q5Q5Q5Q5Q5Q5Q5Q5Q5Q5Q5Q5Q5Q5Q";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
